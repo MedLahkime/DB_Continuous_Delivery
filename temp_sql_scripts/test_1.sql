65d1f7dc1b135c2e1e9ec541ba3410344a9cae3c
MedLahkime <38785080+MedLahkime@users.noreply.github.com>
2020-07-06 11:51:02 +0100

    Update test_1.sql

--- temp_sql_scripts/test_1.sql
+++ temp_sql_scripts/test_1.sql
@@ -1,3 +1,10 @@
-use db;
-ALTER TABLE place
-  ADD lane int NOT NULL DEFAULT '0';
+use classicmodels;
+insert  into `customers`(`customerNumber`,`customerName`,`contactLastName`,`contactFirstName`,`phone`,`addressLine1`,`addressLine2`,`city`,`state`,`postalCode`,`country`,`salesRepEmployeeNumber`,`creditLimit`) values 
+
+(559,'Atelier graphique','Schmitt','Carine ','40.32.2555','54, rue Royale',NULL,'Nantes',NULL,'44000','France',1370,'21000.00'),
+
+(560,'Signal Gift Stores','King','Jean','7025551838','8489 Strong St.',NULL,'Las Vegas','NV','83030','USA',1166,'71800.00'),
+
+(561,'Australian Collectors, Co.','Ferguson','Peter','03 9520 4555','636 St Kilda Road','Level 3','Melbourne','Victoria','3004','Australia',1611,'117300.00'),
+
+(562,'La Rochelle Gifts','Labrune','Janine ','40.67.8555','67, rue des Cinquante Otages',NULL,'Nantes',NULL,'44000','France',1370,'118200.00');
